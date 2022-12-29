591cc34109a23b8bb6b2d7caf77381243e7e6f32
IZ3r0I <denisbahia@live.com.ar>
2022-12-29 12:16:07 -0300

    Se agrega el Proyecto

---
+++ build/web/js/imagenes.js
@@ -0,0 +1,15 @@
+
+
+ document.addEventListener("click",function (e){
+   if(e.target.classList.contains("gallery-item")){
+   	  const src = e.target.getAttribute("src");
+   	  document.querySelector(".modal-img").src = src;
+   	  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+   	  myModal.show();
+   }
+ })
+
+
+/*  $(document).ready(function()
+ {$("img").click(function()
+ {this.requestFullscreen()})}) */
